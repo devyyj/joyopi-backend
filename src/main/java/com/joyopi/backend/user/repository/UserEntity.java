b53078608f71bcf9832a694d3d50c173e69a6136
devyyj <devyyj@gmail.com>
2025-01-20 08:54:45 +0900

    소프트 삭제 적용

--- src/main/java/com/joyopi/backend/user/repository/UserEntity.java
+++ src/main/java/com/joyopi/backend/user/repository/UserEntity.java
@@ -1,28 +1,35 @@
 package com.joyopi.backend.user.repository;
 
 import com.joyopi.backend.common.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.Where;
 
 import java.time.LocalDateTime;
 
 @Data
 @Entity
 @EqualsAndHashCode(callSuper = true)
+@SQLDelete(sql = "UPDATE users SET deleted = true WHERE id = ?") // DELETE 대신 UPDATE 실행
+@SQLRestriction("deleted = false") // 조회 시 삭제된 데이터 제외
 @Table(name = "users",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = {"oauthProvider", "oauthId"})
         }
 )
 public class UserEntity extends BaseEntity {
+
     @Column(length = 20)
     private String nickName;
+
     private String oauthProvider;
     private String oauthId;
     private String role;
     private LocalDateTime lastLogin;
+
+    @Column(name = "deleted", nullable = false)
+    private boolean deleted = false; // 소프트 삭제 플래그
 }
